1a46b890fb3b5ae6ad87fc47574ffeeb3facacf6
rrrrrrr <rrrrrrrrr@gmail.com>
2021-03-04 18:10:37 -0500

    Rendering Classes Dynamically

--- counter-app/src/components/counter.js
+++ counter-app/src/components/counter.js
@@ -6,11 +6,17 @@ class Counter extends Component {
     render() {
         return (
           <div>
-            <h1 className="badge badge-primary m-2">{this.foratCount()}</h1>
+            <h1 className={this.getBadgeClasses()}>{this.foratCount()}</h1>
             <button className="btn btn-secondary btn-sm">Inc</button>
           </div>
         );
     }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
+  }
 
     foratCount(){
       const {count} = this.state;

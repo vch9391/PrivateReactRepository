7836c904ba1f7d86da575039711db070c25a8c1f
rrrrrrr <rrrrrrrrr@gmail.com>
2021-03-04 17:50:16 -0500

    Setting Attributes

--- counter-app/src/components/counter.js
+++ counter-app/src/components/counter.js
@@ -6,8 +6,8 @@ class Counter extends Component {
     render() {
         return (
           <div>
-            <h1>{this.foratCount()}</h1>
-            <button>Inc</button>
+            <h1 className="badge badge-primary m-2">{this.foratCount()}</h1>
+            <button className="btn btn-secondary btn-sm">Inc</button>
           </div>
         );
     }

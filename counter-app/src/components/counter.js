b40ec4037690ed28c0851bd8158534c8a4cf9ea8
rrrrrrr <rrrrrrrrr@gmail.com>
2021-03-04 17:41:23 -0500

    Embedding Expressions

--- counter-app/src/components/counter.js
+++ counter-app/src/components/counter.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  state = { count: 0 };
+
     render() {
         return (
           <div>
-            <h1>Helle World...</h1>
+            <h1>{this.foratCount()}</h1>
             <button>Inc</button>
           </div>
         );
+    }
+
+    foratCount(){
+      const {count} = this.state;
+      return count === 0 ? "Zero" : count;
     }
 }
  

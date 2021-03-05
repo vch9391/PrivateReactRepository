00f347ad27fc552f38b7186176da95a0552237f8
rrrrrrr <rrrrrrrrr@gmail.com>
2021-03-04 19:07:18 -0500

    Reverse - 2 commits

--- counter-app/src/components/counter.js
+++ counter-app/src/components/counter.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = { 
-    tags:[]
-  };
-  
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There is no tags</p>
-    return <ui>{this.state.tags.map(o=> <li key={o}> {o} </li> )}</ui>
-  }
+  state = { count: 0 };
 
-  render() {
-    return (
-    <div>
-      {this.state.tags.length === 0 && "Please create new Tags"}
-      {this.renderTags()}
-    </div>
-    );
-  }
+    render() {
+        return (
+          <div>
+            <h1 className="badge badge-primary m-2">{this.foratCount()}</h1>
+            <button className="btn btn-secondary btn-sm">Inc</button>
+          </div>
+        );
+    }
+
+    foratCount(){
+      const {count} = this.state;
+      return count === 0 ? "Zero" : count;
+    }
 }
  
 export default Counter;

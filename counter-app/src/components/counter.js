81595381ce28c6742c6c129eb5a1196bcaea1cb4
rrrrrrr <rrrrrrrrr@gmail.com>
2021-03-04 19:54:10 -0500

    Passing Event Arguments

--- counter-app/src/components/counter.js
+++ counter-app/src/components/counter.js
@@ -12,14 +12,15 @@ class Counter extends Component {
   //   console.log("Increment called",this);
   // }
 
-  handleIncrement = () => {
+   handleIncrement = (product) => {
+    console.log(product);
     this.setState({count: this.state.count+1});
   }
     render() {
         return (
           <div>
             <h1 className="badge badge-primary m-2">{this.foratCount()}</h1>
-            <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Inc</button>
+            <button onClick={()=>this.handleIncrement({id:1})} className="btn btn-secondary btn-sm">Inc</button>
           </div>
         );
     }

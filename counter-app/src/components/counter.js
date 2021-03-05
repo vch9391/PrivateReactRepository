d089271fb5a81320a83292a1bcc754d914a4d388
rrrrrrr <rrrrrrrrr@gmail.com>
2021-03-05 17:34:11 -0500

    Fixed the bugs

--- counter-app/src/components/counter.js
+++ counter-app/src/components/counter.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  value = { count: this.props.value };
-
-   handleIncrement = (product) => {
+  state = { count: this.props.count };
+  
+  handleIncrement = (product) => {
     console.log(product);
-    this.setState({value: this.value.count+1});
+    this.setState({count: this.state.count+1});
   }
-    render() {
-        return (
-          <div>
-            <h1 className="badge badge-primary m-2">{this.foratCount()}</h1>
-            <button onClick={()=>this.handleIncrement({id:1})} className="btn btn-secondary btn-sm">Inc</button>
-          </div>
-        );
-    }
-
-    foratCount(){
-      const {count} = this.value;
-      return count === 0 ? "Zero" : count;
-    }
+  
+  render() {
+    return (
+    <div>
+      <h1 className={this.getBadgeClasses()}>{this.foratCount()}</h1>
+      <button onClick={()=>this.handleIncrement({id:1})} className="btn btn-secondary btn-sm">Inc</button>
+    </div>
+    );
+  }
+  
+  foratCount(){
+    const {count} = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+  
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 }
  
 export default Counter;
--- counter-app/src/components/counters.js
+++ counter-app/src/components/counters.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import Counter from './counter'
 
 class Counters extends Component {
-    value = {
+    values = {
         counters : [
-            { id: 1, value: 0 },
-            { id: 2, value: 4 },
-            { id: 3, value: 0 },
-            { id: 4, value: 0 },
-            { id: 4, value: 0 }
+            { id: 1, count: 0 },
+            { id: 2, count: 4 },
+            { id: 3, count: 0 },
+            { id: 4, count: 0 },
+            { id: 4, count: 0 }
         ]
     };
 
     render() {
         return (
             <div>
-                {this.value.counters.map( o => <Counter key={o.id} value={o.value} selectrd={true} />)}
+                {this.values.counters.map( o => <Counter key={o.id} count={o.count} selectrd={true} />)}
             </div>
         );
     }

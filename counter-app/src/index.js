d4173c42ad1ffb4c55c628161ec43e8ee591b356
rrrrrrr <rrrrrrrrr@gmail.com>
2021-03-05 21:20:28 -0500

    Multiple Components in Sync

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css";
-import Counteraa from "./components/counters";
 
-ReactDOM.render(<Counteraa/> ,document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

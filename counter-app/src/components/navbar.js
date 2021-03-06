8b7e38ed77f3c7fcf0048b0f6fb258288e88817b
DESKTOP-B2E5JMQ\chand <vch9393@gmail.com>
2021-03-06 15:01:57 -0500

    Stateless Functional Components

--- counter-app/src/components/navbar.js
+++ counter-app/src/components/navbar.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    render() {
-        return (
-        <nav class="navbar navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                    Navbar{" "}
-                    <span className="badge badge-pill badge-secondary">
-                        {this.props.totalCounters}
-                    </span>
-                </a>
-            </div>
-        </nav>     
-        );
-    }
+const NavBar = (props) => {
+    return (
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                Navbar{" "}
+                <span className="badge badge-pill badge-secondary">
+                    {props.totalCounters}
+                </span>
+            </a>
+        </div>
+    </nav>     
+    );
 }
    
 export default NavBar;

b1be9ae7093acdbe985470d068ef268524ce4d5b
DESKTOP-B2E5JMQ\chand <vch9393@gmail.com>
2021-03-06 15:23:28 -0500

    Fixed bug 2

--- counter-app/src/components/navbar.js
+++ counter-app/src/components/navbar.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
 
 const NavBar = ({totalCounters}) => {
     return (
     <nav className="navbar navbar-light bg-light">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">
+            <a className="navbar-brand" href="/#">
                 Navbar{" "}
                 <span className="badge badge-pill badge-secondary">
                     {totalCounters}
